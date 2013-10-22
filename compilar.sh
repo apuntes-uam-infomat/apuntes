a03ead320143006e57005a9c4841cd404a8a6e19
VicdeJuan <dejuan.victor@gmail.com>
2013-10-22 17:15:59 +0200

    Todos los mathds cambiados por mathbb. Script para subir a dropbox los pdfs

--- compilar.sh
+++ compilar.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
-cd "Cosas guays LaTeX"
-sudo ./install
-cd ..
-cd "Analisis Matematico"
-pdflatex Analisis_Matematico.tex
-cp Analisis_Matematico.pdf ..
-cd ..
-cd "Estructuras Algebraicas"
-pdflatex Apuntes.tex
-cp Apuntes.pdf ..
-cd ..
-mv Apuntes.pdf Estructuras.pdf
-cd "Estadistica I"
-pdflatex EI.tex
-cp EI.pdf ..
+if [ "$#" -eq	"2" ]
+ then
+read -p "Compilar (y/n)?" yn
+case $yn in
+	[Yy]* ) echo "Compilando"
+		cd "Cosas guays LaTeX"
+		sudo ./install
+		cd ..
+		cd "Analisis Matematico"
+		pdflatex Analisis_Matematico.tex
+		cd ..
+		cd "Estructuras Algebraicas"
+		pdflatex Apuntes.tex
+		cp Apuntes.pdf ..
+		cd ..
+		cd "Estadistica I"
+		pdflatex EI.tex;;
+	[Nn]* ) echo "Ok";;
+        * ) echo "Please answer yes or no.";;
+    esac
+echo "Subiendo pdfs a Dropbox (en el directorio de vdj)"
+cp "Analisis Matematico"/Analisis_Matematico.pdf .
+cp "Estadistica I"/EI.pdf .
+cp "Estructuras Algebraicas"/Apuntes.pdf .
+mv Apuntes.pdf Est_Alg.pdf
+cp *.pdf ~/Compartido/Dropbox/Doble\ Grado\ UAM\ \(1\)/TERCEROGILIS/Primer\ Cuatrimestre/Apuntes\ Latex/
+else
+echo "Solo funciono en el ordenador de dejuan"
+echo "troool xD"
+fi
+
