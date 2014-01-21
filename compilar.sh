417cfe900b847513410c8db2750dc9372a799ea9
Guillermo Julián <gjulian93@gmail.com>
2014-01-21 12:05:34 +0100

    Cambios en el script de compilación.

--- compilar.sh
+++ compilar.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-install=false
+install=true
+logfile="compilar.log"
 
 makelatex() {
 	includes="../Cosas guays LaTeX"
 
-	echo "Compilando $1"
 	cd "$1"
 	if $install ; then
-		cp "$includes/*.sty" ./
-		cp "$includes/*.cls" ./
-	fi
-	latexmk -pdf -silent -c && cp *.pdf ..
+		echo "Copiando archivos temporales..."
+		cp $includes/*.sty ./
+		cp $includes/*.cls ./
+	fi	
+	echo "Compilando $1"
+	latexmk -pdf -silent >> $logfile 2>&1 && cp *.pdf ../output || echo ">>>>>>>> Error procesando $1 <<<<<<<<"
 	if $install ; then
+		echo "Eliminando archivos temporales..."
 		rm *.sty
 		rm *.cls
 	fi
@@ -26,29 +29,44 @@ case $iyn in
 		cd "Cosas guays LaTeX"
 		sudo ./install > /dev/null
 		cd ..
-		install=true;;
+		install=false;;
 	esac
 
 
 read -p "Compilar (y/n)? " yn
 case $yn in
 	[Yy]* ) echo "Compilando..."
+		echo > $logfile
+		mkdir -p output
 		echo "-----------------------------------------"
-		makelatex "Analisis Matematico"
-		makelatex "Estructuras Algebraicas"
-		makelatex "Estadistica I";;
+		IFS=$'\n'
+
+		for dir in `ls`
+		do
+        	if [[ -d "$dir" && "$dir" != *LaTeX && "$dir" != "output" ]]; then
+                makelatex "$dir"
+        	fi
+		done
+		;;
 	[Nn]* ) echo "Ok";;
         * ) echo "???";;
     esac
+
 echo "-----------------------------------------"
-if [ $USER=vicdejuan ]
-then
-	echo "Eres dejuan y me se tu directorio"
-	cp *.pdf /home/vicdejuan/Compartido/Dropbox/Doble\ Grado\ UAM\ \(1\)/TERCEROGILIS/Primer\ Cuatrimestre/Apuntes\ Latex/
-else
-	echo "Escribe el path donde quieres copiar los pdfs (a partir de home):"
-	read directory
-	cp *.pdf ~/$directory/
-fi
-echo "Listo."
+
+read -p "Copiar? (y/n)? " yn
+case $yn in
+	[Yy]* ) if [ $USER="vicdejuan" ]
+			then
+				echo "Eres dejuan y me se tu directorio"
+				cp output/*.pdf /home/vicdejuan/Compartido/Dropbox/Doble\ Grado\ UAM\ \(1\)/TERCEROGILIS/Primer\ Cuatrimestre/Apuntes\ Latex/
+			else
+				echo "Escribe el path donde quieres copiar los pdfs (a partir de home):"
+				read directory
+				cp output/*.pdf ~/$directory/
+			fi
+			echo "Listo."
+			;;
+		* ) echo "Ok";;
+esac
 
